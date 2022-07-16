2d5ee40b2d05767bde30c35b72a63a82ab8e0a29
MIracleyin <miracleyin@live.com>
2022-07-16 17:28:29 +0800

    todo: pinsage, tp-gnn

--- model/pinsage.py
+++ model/pinsage.py
@@ -1,9 +1,99 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import dgl.nn.pytorch as dglnn
+import dgl.function as fn
 
 from recbole.model.abstract_recommender import GeneralRecommender
 from recbole.model.init import xavier_normal_initialization
 from recbole.model.loss import BPRLoss
 from recbole.utils import InputType
 
-class Wei
+
+class WeightedSAGEConv(nn.Module):
+    def __init__(self, input_dims, hidden_dims, output_dims, act=F.relu):
+        super(WeightedSAGEConv, self).__init__()
+        self.act = act
+        self.Q = nn.Linear(input_dims, hidden_dims)
+        self.W = nn.Linear(input_dims + hidden_dims, output_dims)
+        self.reset_parameters()
+        self.dropout = nn.Dropout(0.5)
+
+    def reset_parameters(self):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.xavier_uniform_(self.Q.weight, gain=gain)
+        nn.init.xavier_uniform_(self.W.weight, gain=gain)
+        nn.init.constant_(self.Q.bias, 0)
+        nn.init.constant_(self.W.bias, 0)
+
+    def forward(self, g, h, weights):
+        """
+        g : graph
+        h : node features
+        weights : scalar edge weights
+        """
+        h_src, h_dst = h
+        with g.local_scope():
+            g.srcdata['n'] = self.act(self.Q(self.dropout(h_src)))
+            g.edata['w'] = weights.float()
+            g.update_all(fn.u_mul_e('n', 'w', 'm'), fn.sum('m', 'n'))
+            g.update_all(fn.copy_e('w', 'm'), fn.sum('m', 'ws'))
+            n = g.dstdata['n']
+            ws = g.dstdata['ws'].unsqueeze(1).clamp(min=1)
+            z = self.act(self.W(self.dropout(torch.cat([n / ws, h_dst], 1))))
+            z_norm = z.norm(2, 1, keepdim=True)
+            z_norm = torch.where(z_norm == 0, torch.tensor(1.).to(z_norm), z_norm)
+            z = z / z_norm
+            return z
+
+class SAGENet(nn.Module):
+    def __init__(self, hidden_dims, n_layers):
+        """
+        g : DGLHeteroGraph
+            The user-item interaction graph.
+            This is only for finding the range of categorical variables.
+        item_textsets : torchtext.data.Dataset
+            The textual features of each item node.
+        """
+        super().__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(n_layers):
+            self.convs.append(WeightedSAGEConv(hidden_dims, hidden_dims, hidden_dims))
+
+    def forward(self, blocks, h):
+        for layer, block in zip(self.convs, blocks):
+            h_dst = h[:block.num_nodes('DST/' + block.ntypes[0])]
+            h = layer(block, (h, h_dst), block.edata['weights'])
+        return h
+
+class PinSage(GeneralRecommender):
+    input_type = InputType.PAIRWISE
+
+    def __init__(self, config, dataset):
+        super(PinSage, self).__init__(config, dataset)
+
+        # load parameters info
+        self.hidden_dims = config['hidden_size']
+        self.num_layers = config['num_layers']
+
+        # define layers and loss
+        self.user_embedding = nn.Embedding(self.n_users, self.embedding_size)
+        self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
+        self.sege = SAGENet(self.hidden_dims, self.num_layers)
+
+        # parameters initialization
+        self.apply(xavier_normal_initialization)
+
+    def forward(self):
+        pass
+
+    def calculate_loss(self, interaction):
+        pass
+
+    def predict(self, interaction):
+        pass
+
+    def full_sort_predict(self, interaction):
+        pass
+
+
